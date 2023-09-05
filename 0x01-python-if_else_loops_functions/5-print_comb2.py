97043f6bbf91e0316e1aa8188768ff1be6a3a6f1
GLetiwa <graceletiwa@gmail.com>
2023-09-05 13:34:35 +0300

    first commit

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    if number < 99:
-        print(f"{number:02}", end=', ')
-    else:
-        print(f"{number:d}")
+for number in range(99 + 1):
+    print("{:02}".format(number), end=", " if number != 99 else "\n")
