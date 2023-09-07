92b092355dffb0e6661493d1b332a4235823005f
GLetiwa <graceletiwa@gmail.com>
2023-09-07 16:57:49 +0300

    second

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,8 +2,8 @@
 from calculator_1 import add, sub, mul, div
 import sys
 
-def main():
 
+def main():
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
