f15fe4c36583ae7abcdc2cbd99d4ed3caba1e568
GLetiwa <graceletiwa@gmail.com>
2023-09-10 09:44:15 +0300

    project 2

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def print_list_integer(my_list=[]):
-        if not my_list:
-            return
-        for i in range(len(my_list)):
-            print("{:d}".format(i))
+def print_list_integer(my_list=[]):
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
