3cb52e564a4592613fb97810e078fed7ca85de31
GLetiwa <graceletiwa@gmail.com>
2023-09-10 10:02:12 +0300

    project 2

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence:
-        return (None, None)
+    if sentence == "":
+        return (0, None)
     x = len(sentence)
     y = sentence[0]
     return (x, y)
