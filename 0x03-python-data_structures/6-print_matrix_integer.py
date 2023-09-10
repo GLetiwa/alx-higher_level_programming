c7d8ca23e9cb65c211ab0affb1de2d789b8ede10
GLetiwa <graceletiwa@gmail.com>
2023-09-10 09:59:37 +0300

    project 2

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col in row:
-            if col != row[-1]:
-                print("{:d}".format(col), end=" ")
-            print()
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
+        print()
