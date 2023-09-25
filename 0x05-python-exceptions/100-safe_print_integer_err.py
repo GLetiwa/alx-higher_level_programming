8dc5616887c5c5ff138fce919264a6406d105afc
GLetiwa <graceletiwa@gmail.com>
2023-09-25 14:05:24 +0300

    first

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except (ValueError, TypeError):
-        print("Exception:", file=sys.stderr)
+    except (ValueError, TypeError) as e:
+        print("Exception:", e, file=sys.stderr)
         return False
