2cc77cc21d43cec65febb21673972c8024ae0671
GLetiwa <graceletiwa@gmail.com>
2023-09-26 15:09:55 +0300

    first

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """
+
+    Defines a square by its size
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """
+
+        Initializes a square instance.
+
+
+        Args:
+            size (int): The size of the square.
+
+
+        """
+
+        self.__size = size
