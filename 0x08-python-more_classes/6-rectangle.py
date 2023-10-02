61b24036e6058e3dcd8b8c7ade0eb07d90632afa
GLetiwa <graceletiwa@gmail.com>
2023-10-02 13:31:59 +0300

    project 2

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -8,9 +8,6 @@ class Rectangle:
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
-        self.__width = 0
-        self.__height = 0
-
         type(self).number_of_instances += 1
         self.width = width
         self.height = height
