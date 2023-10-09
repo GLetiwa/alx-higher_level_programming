a9ac153fe8f999ed41f7245525613925a380d115
GLetiwa <graceletiwa@gmail.com>
2023-10-09 19:37:16 +0300

    trial 1

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""defines a class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """represents a rect using BaseGeometry"""
+    def __init__(self, width, height):
+        """Intialize a new Rectangle"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
