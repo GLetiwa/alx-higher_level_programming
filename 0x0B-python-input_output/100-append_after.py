48e94145e84d9f1881b6f70c86e92b643c7a6721
GLetiwa <graceletiwa@gmail.com>
2023-10-10 11:54:20 +0300

    project 2

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,6 +3,7 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """inserts line to a text file"""
     test = ""
     with open(filename, "r") as file:
         for line in file:
