58bee8e5f75c31b89c50f53cf9aed2cdd12cec80
GLetiwa <graceletiwa@gmail.com>
2023-10-10 11:58:39 +0300

    project 2

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-"""python file I/O"""
+"""Defines a text file insertion function."""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """inserts line to a text file"""
-    test = ""
-    with open(filename) as file:
-        for line in file:
+    """Insert text after each line containing a given string in a file.
+
+    Args:
+        filename (str): The name of the file.
+        search_string (str): The string to search for within the file.
+        new_string (str): The string to insert.
+    """
+    text = ""
+    with open(filename) as r:
+        for line in r:
             text += line
             if search_string in line:
                 text += new_string
