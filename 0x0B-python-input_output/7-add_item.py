1e0a3c2297db0f57cb0b4bc268dd8aa21b7e884e
GLetiwa <graceletiwa@gmail.com>
2023-10-10 14:35:11 +0300

    project 2

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,16 +3,18 @@
 
 
 import sys
-r = __import__('6-load_from_json_file').load_from_json_file
-w = __import__('5-save_to_json_file').save_to_json_file
 
 
-def json_append(filename):
-    """adds args to a Python list, and saves the file"""
-    try:
-        content = r(filename)
-    except FileNotFoundError:
-        content = []
-    for i in sys.argv[1:]:
-        content.append(i)
-    w(content, filename)
+if __name__ == "__main__":
+    r = __import__('6-load_from_json_file').load_from_json_file
+    w = __import__('5-save_to_json_file').save_to_json_file
+
+    def json_append(filename):
+        """adds args to a Python list, and saves the file"""
+        try:
+            content = r(filename)
+        except FileNotFoundError:
+            content = []
+        for i in sys.argv[1:]:
+            content.append(i)
+        w(content, filename)
