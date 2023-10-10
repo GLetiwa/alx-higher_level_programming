ea02c604397a00defe18d89e1dfce54d5532578b
GLetiwa <graceletiwa@gmail.com>
2023-10-10 11:39:49 +0300

    project 2

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""class definition"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """dict rep of the student"""
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return (self.__dict__)
