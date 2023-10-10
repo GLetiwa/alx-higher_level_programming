4bdd3d1c79e745b80fc46a12a9b355c538bde30d
GLetiwa <graceletiwa@gmail.com>
2023-10-10 11:50:00 +0300

    project 2

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""python file I/O"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    with open(filename, "r") as file:
+        lines = file.readlines()
+
+    with open(filename, "w") as file:
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string + "\n")
