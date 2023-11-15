9f7a772b0a1060cb3d4b48a5c8db1eefed86f7e7
GLetiwa <graceletiwa@gmail.com>
2023-11-15 15:46:13 +0000

    sudo again

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,5 @@
 -- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Use the hbtn_0d_usa database
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL); 
