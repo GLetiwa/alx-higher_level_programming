d119bfefdf9847ecae9692c4c8d49ff219159d4f
GLetiwa <graceletiwa@gmail.com>
2023-11-15 15:19:04 +0000

    sudo again

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
 -- Creates a table force_name on MySQL server.
-CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256)) NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
