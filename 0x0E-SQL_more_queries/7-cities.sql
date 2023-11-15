6eaaba1cee255a9e851db6b2e9bb3c278a710a0c
GLetiwa <graceletiwa@gmail.com>
2023-11-15 16:03:24 +0000

    sudo again

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,11 @@
--- Creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on MySQL server.
+-- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server
+-- creates a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
--- Use hbtn_0d_usa database
+-- use a database
 USE hbtn_0d_usa;
-
-CREATE TABLE IF NOT EXISTS cities (id INT AUTO_INCREMENTED UNIQUE NOT NULL PRIMARY KEY, state_id INT NOT NULL, FOREIGN KEY (state_id) REFERENCES states(id), name VARCHAR(256) NOT NULL); 
+-- creates a table
+CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL,
+state_id INT NOT NULL,
+name VARCHAR(256) NOT NULL,
+PRIMARY KEY(id),
+FOREIGN KEY(state_id) REFERENCES states(id));
