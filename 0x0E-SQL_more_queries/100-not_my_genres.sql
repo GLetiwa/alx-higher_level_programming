641476275e3d2434d08010dcc0c5565cf95ae529
GLetiwa <graceletiwa@gmail.com>
2023-11-16 11:18:10 +0000

    final trial

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,16 @@
+-- Get genres linked to the show Dexter
+SELECT genre_id
+FROM tv_show_genres
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter';
+
+-- List all genres not linked to the show Dexter
+SELECT tv_genres.name
+FROM tv_genres
+WHERE id NOT IN (
+	    SELECT genre_id
+	    FROM tv_show_genres
+	    JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+	    WHERE tv_shows.title = 'Dexter'
+)
+ORDER BY tv_genres.name ASC;
