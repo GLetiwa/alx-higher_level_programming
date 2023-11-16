b99df0022bd8db5f3e48274053ef40604ba638fc
GLetiwa <graceletiwa@gmail.com>
2023-11-16 11:06:52 +0000

    final trial

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- List all shows and linked genres
+SELECT tv_shows.title, tv_genres.name AS genre
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title ASC, genre ASC;
