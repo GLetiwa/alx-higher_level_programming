550a644336ec311fe0b10b2da3203b66844ac521
GLetiwa <graceletiwa@gmail.com>
2023-12-11 16:17:36 +0000

    argv 2 is the beginning

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
-if (process.argv[2] && process.argv[3]) { console.log(`${process.argv[2]} is ${process.argv[3]}`); }
-else if (process.argv[2]) {
+if (process.argv[2] && process.argv[3]) { console.log(`${process.argv[2]} is ${process.argv[3]}`); } else if (process.argv[2]) {
   console.log(`${process.argv[2]} is undefined`);
 } else {
-  console.log("undefined is undefined");
+  console.log('undefined is undefined');
 }
