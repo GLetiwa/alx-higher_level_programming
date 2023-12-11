770a6125cf4c8dc0457d0fb7884688161af0b1ef
GLetiwa <graceletiwa@gmail.com>
2023-12-11 16:08:39 +0000

    argv 2 is the beginning

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const myArgs = process.argv;
-console.log(myArgs[2] + ' is ' + myArgs[3]);
+
+if (process.argv[2] && process.argv[3]) { console.log(`${process.argv[2]} is ${process.argv[3]}`); }
