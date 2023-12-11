29a766572ca4a33b25e576d93aa3227daad85930
GLetiwa <graceletiwa@gmail.com>
2023-12-11 16:31:48 +0000

    argv 2 is the beginning

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// convert first arg to an int
+const number = parseInt(process.argv[2]);
+
+if (!isNaN(number)) {
+  console.log(`My number: ${number}`);
+} else {
+  console.log('Not a number');
+}
