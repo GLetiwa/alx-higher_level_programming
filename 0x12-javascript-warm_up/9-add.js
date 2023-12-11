77ddbb477df5cbe8e9f029519f94f84d90717ecf
GLetiwa <graceletiwa@gmail.com>
2023-12-11 17:29:25 +0000

    argv 2 is the beginning

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+function add (a, b) {
+  return a + b;
+}
+
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
+
+if (!isNaN(num1) && !isNaN(num2)) {
+  console.log(add(num1, num2));
+} else {
+  console.log('NaN');
+}
