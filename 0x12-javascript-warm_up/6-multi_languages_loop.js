41d05a9873879254d5a27505f1fef1e7f981163a
GLetiwa <graceletiwa@gmail.com>
2023-12-11 16:46:49 +0000

    argv 2 is the beginning

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < myVar.length; i++) {
+for (let i = 0; myVar[i]; i++) {
   console.log(myVar[i]);
 }
