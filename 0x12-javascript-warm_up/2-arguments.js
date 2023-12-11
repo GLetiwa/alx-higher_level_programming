bec197fcbbd479e4386e7a56234c7d52bab5972c
GLetiwa <graceletiwa@gmail.com>
2023-12-11 12:36:43 +0000

    JS is the way

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const numArgs = process.argv.length - 2;
+
+if (numArgs === 0)
+{
+	console.log('No Argument');
+}
+else if (numArgs === 1)
+{
+	console.log('Argument found');
+}
+else
+{
+	console.log('Arguments found');
+}
