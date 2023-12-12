2a6134ba007bf26963b38fb57ac8e694691f3f20
GLetiwa <graceletiwa@gmail.com>
2023-12-12 09:21:06 +0000

    Object is never a joke

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-	  if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
-		  this.width = undefined;
-		  this.height = undefined;
-	  } else {
-      this.width = w;
-      this.height = h;
-	  }
+    if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
+      return {};
+    }
+    this.width = w;
+    this.height = h;
   }
 }
-
 module.exports = Rectangle;
