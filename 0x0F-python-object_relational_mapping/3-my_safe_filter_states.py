180338dc45b5ce4b350877aed6a38b661cfea978
GLetiwa <graceletiwa@gmail.com>
2023-12-15 23:03:34 -0800

    lastlt

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,43 +1,17 @@
 #!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    # Check if the correct number of arguments is provided
-    if len(sys.argv) != 5:
-        sys.exit(1)
-
-    username, password, database, state_name = sys.argv[1], sys.argv[2], \
-        sys.argv[3], sys.argv[4]
-
-    connection = None
-    cursor = None
-
-    try:
-        # Connect to the MySQL server
-        connection = MySQLdb.connect(
-            host='localhost',
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-        )
-
-        cur = connection.cursor()
-
-        # Execute the SQL query to retrieve states
-        cur.execute("SELECT * FROM states WHERE name LIKE %s", (state_name,))
-
-        states = cur.fetchall()
-
-        for state in states:
-            print(state)
-
-    except MySQLdb.Error as e:
-        print("Error connecting to MySQL: {}".format(e))
-
-    finally:
-        if cur:
-            cur.close()
-        if connection:
-            connection.close()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    match = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
