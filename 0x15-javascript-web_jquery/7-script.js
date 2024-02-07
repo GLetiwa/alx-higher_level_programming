d3c57359e90028dd8895fd0f105e19cadb9abae7
GLetiwa <graceletiwa@gmail.com>
2024-02-07 17:10:41 +0300

    new js

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,7 @@
+$(function () {
+  $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function (data, textStatus) {
+    if (textStatus === 'success') {
+      $('#character').text(data.name);
+    }
+  });
+});
---
+++ 0x15-javascript-web_jquery/README.md
@@ -0,0 +1,1 @@
+Javascrip-Web jQuery
